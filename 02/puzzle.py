f9baa66128c87ab36ed989d8fcccb0d52b0e939b
Thomas Harty <thomas.peter.harty@gmail.com>
2021-12-05 21:23:33 +0000

    day 2: part II

--- 02/puzzle.py
+++ 02/puzzle.py
@@ -1,13 +1,24 @@
+import numpy as np
+
+
 with open('data.txt') as file:
     data = file.read().splitlines()
 # data = ['forward 5', 'down 5', 'forward 8', 'up 3', 'down 8', 'forward 2']
 
 data = ';'.join(data)
-data = (data.replace('forward', 'position +=')
-            .replace('down', 'depth +=')
-            .replace('up', 'depth -='))
+part_I = (data.replace('forward', 'position +=')
+              .replace('down', 'depth +=')
+              .replace('up', 'depth -='))
 
 depth = 0
 position = 0
-exec(data)
+exec(part_I)
 print(f"Day 2: position {position}, depth {depth}, product {position*depth}")
+
+state = np.array([0, 0, 0])  # aim, pos, depth
+part_II = (data.replace('forward', 'state += np.array([0, 1, state[0]]) * ')
+               .replace('down', 'state[0] +=')
+               .replace('up', 'state[0] -='))
+exec(part_II)
+print(f"Day 2: position {state[1]}, depth {state[2]}, "
+      f"product {np.prod(state[1:])}")
