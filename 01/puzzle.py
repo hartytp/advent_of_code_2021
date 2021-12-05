b68a24a4f1aabc6fa367380741b2a8c19ecab10a
Thomas Harty <thomas.peter.harty@gmail.com>
2021-12-05 20:59:29 +0000

    oops, there is a part II

--- 01/puzzle.py
+++ 01/puzzle.py
@@ -3,5 +3,15 @@ import numpy as np
 
 with open('data.txt') as file:
     data = np.array(file.readlines(), dtype=int)
+# data = np.array([199, 200, 208, 210, 200, 207, 240, 269, 260, 263])  # check data
+
+# Part I
 increased = sum(np.diff(data) > 0)
-print(increased)
+print(f"Part II: {increased} increased measurements")
+
+# Part II
+cumsum = np.cumsum(np.insert(data, 0, 0))
+print(cumsum)
+winsum = cumsum[3:] - cumsum[:-3]
+increased = sum(np.diff(winsum) > 0)
+print(f"Part II: {increased} increased averaged measurements")
